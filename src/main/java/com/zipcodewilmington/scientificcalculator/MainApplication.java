4339ff1db7ed0e20ff6a0b37ccf5d993a08e86e8
Han Lin <hlin@zipcodes-MacBook-Pro-6.local>
2020-02-09 17:53:56 -0500

    changed format on Main App

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -10,138 +10,162 @@ import java.util.Scanner;
 public class MainApplication {
 
     public static void main(String[] args) {
+
         double answer = 0;
         double firstInput = 0;
         double secondInput = 0;
         float thirdInput = 0;
         String input = "";
         double store = 0;
-        while (!input.equals("exit")){
-             input = Console.getStringInput("Enter a command in all CAPS example 'HELP'.");
-            //switch on this value for all possible commands
-            switch (input)
+
+        while (!input.equals("exit")) {
+
+            input = Console.getStringInput("Enter a command in all CAPS example 'HELP'.");
+
+             //switch on this value for all possible commands
+            switch (input.toLowerCase().replace("_", " "))
             {
-                case "CLEAR":
+                case "clear":
                     store = 0;
                     break;
-                case "HELP":
+
+                case "help":
                     System.out.println("CLEAR HELP SWITCH_DISPLAY SWITCH_TRIG " +
                             "STORE MATH RESET RECALL DISPLAY TOGGLE_NEGATIVE");
                     break;
-                case "SWITCH_DISPLAY":
+
+                case "switch display":
                     input = Console.getStringInput("Input which display.");
                     ScientificMethod.DisplayMode.valueOf(input);
                     ScientificMethod.DisplayMode input1 = ScientificMethod.DisplayMode.valueOf(input);
                     ScientificMethod.switchDisplayMode(input1);
+                    break;
+
+                case "switch trig":
 
                     break;
-                case "SWITCH_TRIG":
 
-                    break;
-                case "STORE":
+                case "store":
                     store = answer;
                     System.out.println(store);
                     break;
-                case "MATH":
+
+                case "math":
                     System.out.println("+, -, *, /, square, sqrt, invsqrt," +
                             "sin, cos, tan, invSin, invCos, invTan, log, invLog, factorial");
                     break;
-                case "RESET":
-                    store = 0;
 
+                case "reset":
+                    store = 0;
                     break;
-                case "RECALL":
+
+                case "recall":
                     System.out.println(store);
                     break;
-                case "DISPLAY":
+
+                case "display":
                     System.out.println(store);
                     break;
-                case "TOGGLE_NEGATIVE":
+
+                case "toggle negative":
+                    answer = -answer;
+                    System.out.println(answer);
                     break;
+
                 case "+":
                   firstInput = Console.getDoubleInput("Input first number. ");
                   secondInput = Console.getDoubleInput("Input second number. ");
                   answer = BasicFeatures.add(firstInput, secondInput);
                   System.out.println(answer);
                   break;
+
                 case "-":
                     firstInput = Console.getDoubleInput("Input first number.");
                     secondInput = Console.getDoubleInput("Input second number.");
                     answer = BasicFeatures.minus(firstInput, secondInput);
                     System.out.println(answer);
                     break;
+
                 case "*":
                     firstInput = Console.getDoubleInput("Input first number.");
                     secondInput = Console.getDoubleInput("Input second number.");
                     answer = BasicFeatures.multiple(firstInput, secondInput);
                     System.out.println(answer);
                     break;
+
                 case "/":
                     firstInput = Console.getDoubleInput("Input first number.");
                     secondInput = Console.getDoubleInput("Input second number.");
                     answer = BasicFeatures.division(firstInput, secondInput);
                     System.out.println(answer);
                     break;
+
                 case "square":
                     firstInput = Console.getDoubleInput("Input a number.");
                     answer = ScientificMethod.getSquare(firstInput);
                     System.out.println(answer);
                     break;
+
                 case "invsqrt":
                     thirdInput = Console.getFloatInput("Input a number.");
                     answer = ScientificMethod.invSqrt(thirdInput);
                     System.out.println(answer);
                     break;
+
                 case "sqrt":
                     thirdInput = Console.getFloatInput("Input a number.");
                     answer = ScientificMethod.sqrt(thirdInput);
                     System.out.println(answer);
                     break;
+
                 case "sin":
                     firstInput = Console.getDoubleInput("Input a number.");
                     answer = ScientificMethod.getSin(firstInput);
                     System.out.println(answer);
                     break;
+
                 case "cos":
                     firstInput = Console.getDoubleInput("Input a number");
                     answer = ScientificMethod.getCos(firstInput);
                     System.out.println(answer);
                     break;
+
                 case "tan":
                     firstInput = Console.getDoubleInput("Input a number");
                     answer = ScientificMethod.getTan(firstInput);
                     System.out.println(answer);
                     break;
-                case "invSin":
+
+                case "invsin":
                     firstInput = Console.getDoubleInput("Input a number");
                     answer = ScientificMethod.getInverseSin(firstInput);
                     System.out.println(answer);
                     break;
-                case "invCos":
+
+                case "invcos":
                     firstInput = Console.getDoubleInput("Input a number");
                     answer = ScientificMethod.getInverseCos(firstInput);
                     System.out.println(answer);
                     break;
-                case "invTan":
+
+                case "invtan":
                     firstInput = Console.getDoubleInput("Input a number");
                     answer = ScientificMethod.getInverseTan(firstInput);
                     System.out.println(answer);
                     break;
+
                 case "log":
                     firstInput = Console.getDoubleInput("Input a number");
                     answer = ScientificMethod.getLog(firstInput);
                     System.out.println(answer);
                     break;
+
                 case "factorial":
                     firstInput = Console.getDoubleInput("Input a factorial.");
                     secondInput = Console.getDoubleInput("Input a end value.");
                     answer = ScientificMethod.getFactorial(firstInput, secondInput);
                     System.out.println(answer);
                     break;
-
-
-
-
 
 
             }
